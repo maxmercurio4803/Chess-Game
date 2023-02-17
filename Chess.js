31b891ec782e41e4e6bbbad909de3debfa081c85
maxmercurio4803 <113199862+maxmercurio4803@users.noreply.github.com>
2023-02-17 16:10:01 -0500

    Added all files

---
+++ Chess.js
@@ -0,0 +1,72 @@
+// Inserting the Images
+function insertImage() {
+
+
+}
+insertImage()
+
+
+
+//Coloring
+
+function coloring() {
+
+}
+coloring()
+
+
+
+
+//function to not remove the same team element
+
+function reddish() {
+
+}
+
+
+
+
+
+
+
+
+
+
+tog = 1
+
+document.querySelectorAll('.box').forEach(item => {
+
+
+
+    item.addEventListener('click', function () {
+
+        // To delete the opposite element
+
+        
+
+            // KING
+    })
+
+            
+})
+
+
+
+
+
+// Moving the element
+document.querySelectorAll('.box').forEach(hathiTest => {
+
+
+})
+
+
+
+
+
+
+// Prvents from selecting multiple elements
+z = 0
+document.querySelectorAll('.box').forEach(ee => {
+
+})
